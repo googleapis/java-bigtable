10d6d63ffb7e0b414d883e115556f94e1c6159bd
Kristen O'Leary <kaoleary@google.com>
2019-09-27 14:21:17 -0400

    add headers

--- synth.py
+++ synth.py
@@ -25,8 +25,27 @@ import synthtool.languages.java as java
 def main():
   gapic = gcp.GAPICGenerator()
 
-  generate_data_api(gapic)
-  generate_admin_api(gapic)
+  # License header
+  license = """
+  /*
+   * Copyright 2019 Google LLC
+   *
+   * Licensed under the Apache License, Version 2.0 (the "License");
+   * you may not use this file except in compliance with the License.
+   * You may obtain a copy of the License at
+   *
+   *     https://www.apache.org/licenses/LICENSE-2.0
+   *
+   * Unless required by applicable law or agreed to in writing, software
+   * distributed under the License is distributed on an "AS IS" BASIS,
+   * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   * See the License for the specific language governing permissions and
+   * limitations under the License.
+   */
+  """
+
+  generate_data_api(gapic, license)
+  generate_admin_api(gapic, license)
 
   java.format_code('src')
 
@@ -37,7 +56,7 @@ def main():
     'README.md',
   ])
 
-def generate_data_api(gapic):
+def generate_data_api(gapic, license):
   library = gapic.java_library(
     service='bigtable',
     version='v2',
@@ -59,23 +78,33 @@ def generate_data_api(gapic):
 
   # Paths are relative to the destination, which is the current working directory
   internal_only = [
-    "src/main/java/com/google/cloud/bigtable/data/v2/stub/BigtableStub.java",
-    'src/main/java/com/google/cloud/bigtable/data/v2/stub/BigtableStubSettings.java',
-    "src/main/java/com/google/cloud/bigtable/data/v2/stub/GrpcBigtableStub.java",
-    "src/main/java/com/google/cloud/bigtable/data/v2/stub/GrpcBigtableCallableFactory.java",
+    "google-cloud-bigtable/src/main/java/com/google/cloud/bigtable/data/v2/stub/BigtableStub.java",
+    'google-cloud-bigtable/src/main/java/com/google/cloud/bigtable/data/v2/stub/BigtableStubSettings.java',
+    "google-cloud-bigtable/src/main/java/com/google/cloud/bigtable/data/v2/stub/GrpcBigtableStub.java",
+    "google-cloud-bigtable/src/main/java/com/google/cloud/bigtable/data/v2/stub/GrpcBigtableCallableFactory.java",
   ]
 
-  s.copy(library / 'gapic-google-cloud-bigtable-v2', '.', excludes=excludes)
-  s.copy(library / 'grpc-google-cloud-bigtable-v2/src', '../../google-api-grpc/grpc-google-cloud-bigtable-v2/src')
-  s.copy(library / 'proto-google-cloud-bigtable-v2/src', '../../google-api-grpc/proto-google-cloud-bigtable-v2/src')
+  package_name = f'com.google.cloud.bigtable.v2'
+  java.fix_proto_headers(library / f'proto-google-cloud-bigtable-v2')
+  # java.fix_grpc_headers(library / f'grpc-google-cloud-bigtable-v2', package_name)
+
+  ##todo replace with fix_grpc_headers
+  s.replace(
+      library / f'grpc-google-cloud-bigtable-v2/src/**/*.java',
+      f"package com.google.bigtable.v2;",
+      f"{license}package com.google.bigtable.v2;"
+  )
+  s.copy(library / f'gapic-google-cloud-bigtable-v2', 'google-cloud-bigtable/', excludes=excludes)
+  s.copy(library / f'grpc-google-cloud-bigtable-v2/src', f'grpc-google-cloud-bigtable-v2/src')
+  s.copy(library / f'proto-google-cloud-bigtable-v2/src', f'proto-google-cloud-bigtable-v2/src')
 
   make_internal_only(internal_only)
 
-  java.format_code('../../google-api-grpc/grpc-google-cloud-bigtable-v2/src')
-  java.format_code('../../google-api-grpc/proto-google-cloud-bigtable-v2/src')
+  java.format_code(f'./grpc-google-cloud-bigtable-v2/src')
+  java.format_code(f'./proto-google-cloud-bigtable-v2/src')
 
 
-def generate_admin_api(gapic):
+def generate_admin_api(gapic, license):
   library = gapic.java_library(
       service='bigtable-admin',
       version='v2',
@@ -89,27 +118,37 @@ def generate_admin_api(gapic):
 
   # Paths are relative to the destination, which is the current working directory
   internal_only = [
-    'src/main/java/com/google/cloud/bigtable/admin/v2/stub/GrpcBigtableInstanceAdminCallableFactory.java',
-    'src/main/java/com/google/cloud/bigtable/admin/v2/stub/GrpcBigtableInstanceAdminStub.java',
-    'src/main/java/com/google/cloud/bigtable/admin/v2/BaseBigtableInstanceAdminSettings.java',
-    'src/main/java/com/google/cloud/bigtable/admin/v2/stub/GrpcBigtableTableAdminCallableFactory.java',
-    'src/main/java/com/google/cloud/bigtable/admin/v2/stub/GrpcBigtableTableAdminStub.java',
-    'src/main/java/com/google/cloud/bigtable/admin/v2/BaseBigtableTableAdminSettings.java',
-
-    'src/main/java/com/google/cloud/bigtable/admin/v2/stub/BigtableInstanceAdminStub.java',
-    'src/main/java/com/google/cloud/bigtable/admin/v2/stub/BigtableTableAdminStub.java',
-    'src/main/java/com/google/cloud/bigtable/admin/v2/BaseBigtableInstanceAdminClient.java',
-    'src/main/java/com/google/cloud/bigtable/admin/v2/BaseBigtableTableAdminClient.java',
+    'google-cloud-bigtable/src/main/java/com/google/cloud/bigtable/admin/v2/stub/GrpcBigtableInstanceAdminCallableFactory.java',
+    'google-cloud-bigtable/src/main/java/com/google/cloud/bigtable/admin/v2/stub/GrpcBigtableInstanceAdminStub.java',
+    'google-cloud-bigtable/src/main/java/com/google/cloud/bigtable/admin/v2/BaseBigtableInstanceAdminSettings.java',
+    'google-cloud-bigtable/src/main/java/com/google/cloud/bigtable/admin/v2/stub/GrpcBigtableTableAdminCallableFactory.java',
+    'google-cloud-bigtable/src/main/java/com/google/cloud/bigtable/admin/v2/stub/GrpcBigtableTableAdminStub.java',
+    'google-cloud-bigtable/src/main/java/com/google/cloud/bigtable/admin/v2/BaseBigtableTableAdminSettings.java',
+    'google-cloud-bigtable/src/main/java/com/google/cloud/bigtable/admin/v2/stub/BigtableInstanceAdminStub.java',
+    'google-cloud-bigtable/src/main/java/com/google/cloud/bigtable/admin/v2/stub/BigtableTableAdminStub.java',
+    'google-cloud-bigtable/src/main/java/com/google/cloud/bigtable/admin/v2/BaseBigtableInstanceAdminClient.java',
+    'google-cloud-bigtable/src/main/java/com/google/cloud/bigtable/admin/v2/BaseBigtableTableAdminClient.java',
   ]
 
-  s.copy(library / 'gapic-google-cloud-bigtable-admin-v2/src', 'src', excludes=excludes)
-  s.copy(library / 'grpc-google-cloud-bigtable-admin-v2/src', '../../google-api-grpc/grpc-google-cloud-bigtable-admin-v2/src')
-  s.copy(library / 'proto-google-cloud-bigtable-admin-v2/src', '../../google-api-grpc/proto-google-cloud-bigtable-admin-v2/src')
+  package_name = f'com.google.cloud.bigtable.admin.v2'
+  java.fix_proto_headers(library / f'proto-google-cloud-bigtable-admin-v2')
+  # java.fix_grpc_headers(library / f'grpc-google-cloud-bigtable-admin-v2', package_name)
+
+  ##todo replace with fix_grpc_headers
+  s.replace(
+      library / f'grpc-google-cloud-bigtable-admin-v2/src/**/*.java',
+      f"package com.google.bigtable.admin.v2;",
+      f"{license}package com.google.bigtable.admin.v2;"
+  )
+
+  s.copy(library / f'gapic-google-cloud-bigtable-admin-v2/src', 'google-cloud-bigtable/src', excludes=excludes)
+  s.copy(library / f'grpc-google-cloud-bigtable-admin-v2/src', f'grpc-google-cloud-bigtable-admin-v2/src')
+  s.copy(library / f'proto-google-cloud-bigtable-admin-v2/src', f'proto-google-cloud-bigtable-admin-v2/src')
 
   make_internal_only(internal_only)
 
-  java.format_code('../../google-api-grpc/grpc-google-cloud-bigtable-admin-v2/src')
-  java.format_code('../../google-api-grpc/proto-google-cloud-bigtable-admin-v2/src')
+  java.format_code(f'./grpc-google-cloud-bigtable-admin-v2/src')
+  java.format_code(f'./proto-google-cloud-bigtable-admin-v2/src')
 
 
 def make_internal_only(sources):
