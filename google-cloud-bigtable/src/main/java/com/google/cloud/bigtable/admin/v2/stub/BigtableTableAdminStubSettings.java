292838432d55bae98924a382610b446cc4d689d9
yoshi-automation <yoshi-automation@google.com>
2021-02-09 15:30:43 -0800

    fix: add retryableStatusCodes back to bigtable
    Committer: @miraleung
    PiperOrigin-RevId: 351852297
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Thu Jan 14 12:19:44 2021 -0800
    Source-Repo: googleapis/googleapis
    Source-Sha: df49bfe877fff8c37f60855a7e754b7a4369709e
    Source-Link: https://github.com/googleapis/googleapis/commit/df49bfe877fff8c37f60855a7e754b7a4369709e

--- google-cloud-bigtable/src/main/java/com/google/cloud/bigtable/admin/v2/stub/BigtableTableAdminStubSettings.java
+++ google-cloud-bigtable/src/main/java/com/google/cloud/bigtable/admin/v2/stub/BigtableTableAdminStubSettings.java
@@ -636,7 +636,10 @@ public class BigtableTableAdminStubSettings extends StubSettings<BigtableTableAd
           "no_retry_0_codes", ImmutableSet.copyOf(Lists.<StatusCode.Code>newArrayList()));
       definitions.put("no_retry_codes", ImmutableSet.copyOf(Lists.<StatusCode.Code>newArrayList()));
       definitions.put(
-          "retry_policy_2_codes", ImmutableSet.copyOf(Lists.<StatusCode.Code>newArrayList()));
+          "retry_policy_2_codes",
+          ImmutableSet.copyOf(
+              Lists.<StatusCode.Code>newArrayList(
+                  StatusCode.Code.UNAVAILABLE, StatusCode.Code.DEADLINE_EXCEEDED)));
       definitions.put(
           "no_retry_1_codes", ImmutableSet.copyOf(Lists.<StatusCode.Code>newArrayList()));
       definitions.put(
