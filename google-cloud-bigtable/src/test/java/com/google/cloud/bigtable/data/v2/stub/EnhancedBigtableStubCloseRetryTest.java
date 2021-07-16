6ae2d4de95bf76449456344cd5eaa824626a1323
Igor Bernstein <igorbernstein@google.com>
2021-07-16 18:56:48 -0400

    fix: client.close() should wait until the channels are terminated before shutting down the executor
    Previously the client.close() would simply shutdown the channel pool and the executor immediately after. Unfortunately this leads to RPCs that would hang forever because an outstanding RPC didnt have an executor to notify of its completion. This PR ensures that the channels are drained before shutting down the executor

---
+++ google-cloud-bigtable/src/test/java/com/google/cloud/bigtable/data/v2/stub/EnhancedBigtableStubCloseRetryTest.java
@@ -0,0 +1,105 @@
+package com.google.cloud.bigtable.data.v2.stub;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.google.api.core.ApiFuture;
+import com.google.api.gax.core.NoCredentialsProvider;
+import com.google.bigtable.v2.BigtableGrpc;
+import com.google.bigtable.v2.ReadRowsRequest;
+import com.google.bigtable.v2.ReadRowsResponse;
+import com.google.cloud.bigtable.data.v2.BigtableDataSettings;
+import com.google.cloud.bigtable.data.v2.FakeServiceHelper;
+import com.google.cloud.bigtable.data.v2.models.Query;
+import com.google.cloud.bigtable.data.v2.models.Row;
+import io.grpc.Status;
+import io.grpc.stub.StreamObserver;
+import java.util.List;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.atomic.AtomicInteger;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.function.ThrowingRunnable;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/** Ensures that closing a client during exponential retry will not hang any requests. */
+@RunWith(JUnit4.class)
+public class EnhancedBigtableStubCloseRetryTest {
+  private static final String PROJECT_ID = "fake-project";
+  private static final String INSTANCE_ID = "fake-instance";
+
+  private ExecutorService testExecutor;
+  private BlockingQueue<ReadRowsRequest> requests;
+  private AtomicInteger numRequests;
+
+  private FakeServiceHelper serviceHelper;
+  private EnhancedBigtableStub stub;
+
+  @Before
+  public void setUp() throws Exception {
+    testExecutor = Executors.newCachedThreadPool();
+    requests = new ArrayBlockingQueue<>(10);
+    numRequests = new AtomicInteger();
+
+    serviceHelper = new FakeServiceHelper(new FakeBigtable());
+    serviceHelper.start();
+
+    BigtableDataSettings.Builder settingBuilder =
+        BigtableDataSettings.newBuilderForEmulator(serviceHelper.getPort())
+            .setProjectId(PROJECT_ID)
+            .setInstanceId(INSTANCE_ID)
+            .setCredentialsProvider(NoCredentialsProvider.create())
+            .setRefreshingChannel(false);
+
+    stub = EnhancedBigtableStub.create(settingBuilder.build().getStubSettings());
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    testExecutor.shutdown();
+    stub.close();
+    serviceHelper.shutdown();
+  }
+
+  @Test
+  public void outstandingRequestsFinishAfterClose() throws Exception {
+    final ApiFuture<List<Row>> resultFuture =
+        stub.readRowsCallable().all().futureCall(Query.create("table1"));
+
+    // wait for the first request to hit the server
+    requests.take();
+    // wait enough time for a retry attempt to be scheduled before closing the client
+    Thread.sleep(100);
+    stub.close();
+
+    ExecutionException error =
+        Assert.assertThrows(
+            ExecutionException.class,
+            new ThrowingRunnable() {
+              @Override
+              public void run() throws Throwable {
+                resultFuture.get();
+              }
+            });
+
+    assertThat(error.getCause()).isInstanceOf(RejectedExecutionException.class);
+  }
+
+  class FakeBigtable extends BigtableGrpc.BigtableImplBase {
+    @Override
+    public void readRows(
+        ReadRowsRequest request, StreamObserver<ReadRowsResponse> responseObserver) {
+
+      requests.add(request);
+      // Keep returning a retriable error
+      responseObserver.onError(Status.UNAVAILABLE.asRuntimeException());
+    }
+  }
+}
