2a89d2db698513f0069671f01a01eed4af0d4e20
Mridula Peddada <mpeddada@google.com>
2022-05-11 13:57:03 -0400

    add documentation; rever unnecessary changes

--- google-cloud-bigtable/src/test/java/com/google/cloud/bigtable/admin/v2/BigtableInstanceAdminSettingsTest.java
+++ google-cloud-bigtable/src/test/java/com/google/cloud/bigtable/admin/v2/BigtableInstanceAdminSettingsTest.java
@@ -29,7 +29,7 @@ import org.mockito.Mockito;
 import org.threeten.bp.Duration;
 
 @RunWith(JUnit4.class)
-public class BigtableInstanceAdminSettingsTests {
+public class BigtableInstanceAdminSettingsTest {
   @Test
   public void testProjectName() throws Exception {
     String projectId = "my-project";
