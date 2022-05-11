d4747316e48d045af941204567713f8935167db9
Mridula Peddada <mpeddada@google.com>
2022-05-11 15:22:24 -0400

    resolve conflicts

--- google-cloud-bigtable/src/test/java/com/google/cloud/bigtable/admin/v2/BigtableInstanceAdminClientTests.java
+++ google-cloud-bigtable/src/test/java/com/google/cloud/bigtable/admin/v2/BigtableInstanceAdminClientTests.java
@@ -81,6 +81,11 @@ import org.mockito.quality.Strictness;
 import org.mockito.stubbing.Answer;
 
 @RunWith(JUnit4.class)
+/**
+ * Tests for {@link BigtableTableAdminClient}. This test class uses Mockito so
+ * it has been explicitly excluded from Native Image testing by not following
+ * the naming convention of (IT* and *ClientTest).
+ */
 public class BigtableInstanceAdminClientTests {
   @Rule public MockitoRule mockitoRule = MockitoJUnit.rule().strictness(Strictness.WARN);
 
