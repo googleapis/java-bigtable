c0213c13b03aaa50d2a449d0900a6a8be2874b7a
Billy Jacobson <thebilly@google.com>
2022-09-20 11:09:24 -0400

    fix indentation

--- samples/snippets/src/main/java/com/example/bigtable/KeySalting.java
+++ samples/snippets/src/main/java/com/example/bigtable/KeySalting.java
@@ -26,59 +26,64 @@ public class KeySalting {
   private static final String COLUMN_FAMILY_NAME = "stats_summary";
   public static final int SALT_RANGE = 4;
 
-  public static void writeSaltedRow(String projectId, String instanceId, String tableId, String rowKey) throws IOException {
+  public static void writeSaltedRow(
+      String projectId, String instanceId, String tableId, String rowKey) throws IOException {
     BigtableDataClient dataClient = BigtableDataClient.create(projectId, instanceId);
     String saltedRowKey = getSaltedRowKey(rowKey, SALT_RANGE);
-    RowMutation rowMutation = RowMutation.create(tableId, saltedRowKey).setCell(COLUMN_FAMILY_NAME, "os_build", "PQ2A.190405.003");
+    RowMutation rowMutation =
+        RowMutation.create(tableId, saltedRowKey)
+            .setCell(COLUMN_FAMILY_NAME, "os_build", "PQ2A.190405.003");
 
     dataClient.mutateRow(rowMutation);
     System.out.printf("Successfully wrote row %s as %s\n", rowKey, saltedRowKey);
   }
 
-  public static void readSaltedRow(String projectId, String instanceId, String tableId, String rowKey) throws IOException {
+  public static void readSaltedRow(
+      String projectId, String instanceId, String tableId, String rowKey) throws IOException {
     BigtableDataClient dataClient = BigtableDataClient.create(projectId, instanceId);
     Row row = dataClient.readRow(tableId, getSaltedRowKey(rowKey, SALT_RANGE));
     System.out.printf("Successfully read row %s\n", row.getKey().toStringUtf8());
   }
 
-//    public static void scanSaltedRows(String projectId, String instanceId, String tableId, String prefix) throws IOException {
-//        AccumulatingObserver observer = new AccumulatingObserver();
-//
-//        BigtableDataClient dataClient = BigtableDataClient.create(projectId, instanceId);
-//        Query query = Query.create(tableId).prefix(prefix);
-//        ServerStream<Row> rows = dataClient.readRowsAsync(query, );
-//
-//        bigtableDataClient.readRowsAsync(query, new ResponseObserver<Row>() {
-//            StreamController controller;
-//            int count = 0;
-//            public void onStart(StreamController controller) {
-//                this.controller = controller;
-//            }
-//            public void onResponse(Row row) {
-//                if (++count > 10) {
-//                    controller.cancel();
-//                    return;
-//                }
-//                // Do something with Row
-//            }
-//            public void onError(Throwable t) {
-//                if (t instanceof NotFoundException) {
-//                    System.out.println("Tried to read a non-existent table");
-//                } else {
-//                    t.printStackTrace();
-//                }
-//            }
-//
-//            public void onComplete() {
-//                // Handle stream completion
-//            }
-//        });
-//
-//
-//        for (Row row : rows) {
-//            System.out.printf("Successfully read row %s\n", row.getKey().toStringUtf8());
-//        }
-//    }
+  //    public static void scanSaltedRows(String projectId, String instanceId, String tableId,
+  // String prefix) throws IOException {
+  //        AccumulatingObserver observer = new AccumulatingObserver();
+  //
+  //        BigtableDataClient dataClient = BigtableDataClient.create(projectId, instanceId);
+  //        Query query = Query.create(tableId).prefix(prefix);
+  //        ServerStream<Row> rows = dataClient.readRowsAsync(query, );
+  //
+  //        bigtableDataClient.readRowsAsync(query, new ResponseObserver<Row>() {
+  //            StreamController controller;
+  //            int count = 0;
+  //            public void onStart(StreamController controller) {
+  //                this.controller = controller;
+  //            }
+  //            public void onResponse(Row row) {
+  //                if (++count > 10) {
+  //                    controller.cancel();
+  //                    return;
+  //                }
+  //                // Do something with Row
+  //            }
+  //            public void onError(Throwable t) {
+  //                if (t instanceof NotFoundException) {
+  //                    System.out.println("Tried to read a non-existent table");
+  //                } else {
+  //                    t.printStackTrace();
+  //                }
+  //            }
+  //
+  //            public void onComplete() {
+  //                // Handle stream completion
+  //            }
+  //        });
+  //
+  //
+  //        for (Row row : rows) {
+  //            System.out.printf("Successfully read row %s\n", row.getKey().toStringUtf8());
+  //        }
+  //    }
 
   public static String getSaltedRowKey(String rowKey, int saltRange) {
     int prefix = rowKey.hashCode() % saltRange;
