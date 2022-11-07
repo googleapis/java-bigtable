e360b94874a691fced2ce78954d90e18ecfef851
Mattie Fu <mattiefu@google.com>
2022-11-07 12:44:48 -0500

    fix: call record attempt compeletion on permanent failures

--- google-cloud-bigtable/src/test/java/com/google/cloud/bigtable/data/v2/stub/metrics/BuiltinMetricsTracerTest.java
+++ google-cloud-bigtable/src/test/java/com/google/cloud/bigtable/data/v2/stub/metrics/BuiltinMetricsTracerTest.java
@@ -26,6 +26,7 @@ import static org.mockito.Mockito.when;
 import com.google.api.client.util.Lists;
 import com.google.api.core.SettableApiFuture;
 import com.google.api.gax.rpc.ClientContext;
+import com.google.api.gax.rpc.NotFoundException;
 import com.google.api.gax.rpc.ResponseObserver;
 import com.google.api.gax.rpc.StreamController;
 import com.google.api.gax.tracing.SpanName;
@@ -66,6 +67,7 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -85,6 +87,8 @@ public class BuiltinMetricsTracerTest {
   private static final String INSTANCE_ID = "fake-instance";
   private static final String APP_PROFILE_ID = "default";
   private static final String TABLE_ID = "fake-table";
+
+  private static final String BAD_TABLE_ID = "non-exist-table";
   private static final String ZONE = "us-west-1";
   private static final String CLUSTER = "cluster-0";
   private static final long FAKE_SERVER_TIMING = 50;
@@ -345,6 +349,35 @@ public class BuiltinMetricsTracerTest {
     assertThat(status.getAllValues()).containsExactly("UNAVAILABLE", "UNAVAILABLE", "OK");
   }
 
+  @Test
+  public void testPermanentFailure() {
+    when(mockFactory.newTracer(any(), any(), any()))
+        .thenReturn(
+            new BuiltinMetricsTracer(
+                OperationType.ServerStreaming,
+                SpanName.of("Bigtable", "ReadRows"),
+                statsRecorderWrapper));
+
+    try {
+      Lists.newArrayList(stub.readRowsCallable().call(Query.create(BAD_TABLE_ID)).iterator());
+      Assert.fail("Request should throw not found error");
+    } catch (NotFoundException e) {
+    }
+
+    ArgumentCaptor<Long> attemptLatency = ArgumentCaptor.forClass(Long.class);
+    ArgumentCaptor<Long> operationLatency = ArgumentCaptor.forClass(Long.class);
+
+    verify(statsRecorderWrapper, timeout(50)).putAttemptLatencies(attemptLatency.capture());
+    verify(statsRecorderWrapper, timeout(50)).putOperationLatencies(operationLatency.capture());
+    verify(statsRecorderWrapper, timeout(50))
+        .recordAttempt(status.capture(), tableId.capture(), zone.capture(), cluster.capture());
+
+    assertThat(status.getValue()).isEqualTo("NOT_FOUND");
+    assertThat(tableId.getValue()).isEqualTo(BAD_TABLE_ID);
+    assertThat(cluster.getValue()).isEqualTo("unspecified");
+    assertThat(zone.getValue()).isEqualTo("global");
+  }
+
   private static class FakeService extends BigtableGrpc.BigtableImplBase {
 
     static List<ReadRowsResponse> createFakeResponse() {
@@ -375,6 +408,10 @@ public class BuiltinMetricsTracerTest {
     @Override
     public void readRows(
         ReadRowsRequest request, StreamObserver<ReadRowsResponse> responseObserver) {
+      if (request.getTableName().contains(BAD_TABLE_ID)) {
+        responseObserver.onError(new StatusRuntimeException(Status.NOT_FOUND));
+        return;
+      }
       final AtomicBoolean done = new AtomicBoolean();
       final ServerCallStreamObserver<ReadRowsResponse> target =
           (ServerCallStreamObserver<ReadRowsResponse>) responseObserver;
