f4c5fb9c27611656b37cf4cddc584b1c0a1b8875
Diego Gomez <diegomez@google.com>
2023-01-17 18:36:40 +0000

    Removed enable settings and fixed retry tests

--- google-cloud-bigtable/src/main/java/com/google/cloud/bigtable/data/v2/stub/EnhancedBigtableStubSettings.java
+++ google-cloud-bigtable/src/main/java/com/google/cloud/bigtable/data/v2/stub/EnhancedBigtableStubSettings.java
@@ -268,7 +268,14 @@ public class EnhancedBigtableStubSettings extends StubSettings<EnhancedBigtableS
   /** Returns a builder for the default ChannelProvider for this service. */
   public static InstantiatingGrpcChannelProvider.Builder defaultGrpcTransportProviderBuilder() {
     return InstantiatingGrpcChannelProvider.newBuilder()
-        .setMaxInboundMessageSize(Integer.MAX_VALUE)
+        .setPoolSize(getDefaultChannelPoolSize())
+        .setMaxInboundMessageSize(MAX_MESSAGE_SIZE)
+        .setKeepAliveTime(Duration.ofSeconds(30)) // sends ping in this interval
+        .setKeepAliveTimeout(
+            Duration.ofSeconds(10)) // wait this long before considering the connection dead
+        // Attempts direct access to CBT service over gRPC to improve throughput,
+        // whether the attempt is allowed is totally controlled by service owner.
+        .setAttemptDirectPath(true)
         .setChannelConfigurator(
             new FeatureFlagChannelConfigurator(
                 null, FEATURE_FLAGS));
