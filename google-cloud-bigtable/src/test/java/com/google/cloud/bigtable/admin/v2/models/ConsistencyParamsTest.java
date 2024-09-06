c7fbb27d0658cdaea94149fcc8cb649b31e52f86
Derek Yau <derekyau@google.com>
2024-09-06 16:16:48 -0400

    Add tests for ConsistencyRequest model
    Change-Id: I3548b7aa673be5a92cd4c180e3edb8649657811c

--- google-cloud-bigtable/src/test/java/com/google/cloud/bigtable/admin/v2/models/ConsistencyParamsTest.java
+++
@@ -1,8 +0,0 @@
-package com.google.cloud.bigtable.admin.v2.models;
-
-import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
-
-@RunWith(JUnit4.class)
-public class ConsistencyParamsTest {
-}
---
+++ google-cloud-bigtable/src/test/java/com/google/cloud/bigtable/admin/v2/models/ConsistencyRequestTest.java
@@ -0,0 +1,56 @@
+package com.google.cloud.bigtable.admin.v2.models;
+
+import com.google.bigtable.admin.v2.CheckConsistencyRequest;
+import com.google.bigtable.admin.v2.GenerateConsistencyTokenRequest;
+import com.google.bigtable.admin.v2.GenerateConsistencyTokenResponse;
+import com.google.cloud.bigtable.data.v2.internal.RequestContext;
+import org.junit.Test;
+import org.junit.runner.Request;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import static com.google.common.truth.Truth.assertThat;
+
+@RunWith(JUnit4.class)
+public class ConsistencyRequestTest {
+    private final String PROJECT_ID = "my-project";
+    private final String INSTANCE_ID = "my-instance";
+    private final String TABLE_ID = "my-table";
+    private final String CONSISTENCY_TOKEN = "my-token";
+    @Test
+    public void testToCheckConsistencyProtoWithStandard() {
+        ConsistencyRequest consistencyRequest = ConsistencyRequest.getStandardConsistencyRequest(TABLE_ID);
+
+        RequestContext requestContext = RequestContext.create(PROJECT_ID, INSTANCE_ID, TABLE_ID);
+
+        CheckConsistencyRequest checkConsistencyRequest = consistencyRequest.toCheckConsistencyProto(requestContext, CONSISTENCY_TOKEN);
+
+        assertThat(checkConsistencyRequest.getName().equals(TABLE_ID));
+        assertThat(checkConsistencyRequest.getConsistencyToken().equals(CONSISTENCY_TOKEN));
+        assertThat(checkConsistencyRequest.getModeCase().equals(CheckConsistencyRequest.ModeCase.STANDARD_READ_REMOTE_WRITES));
+    }
+
+    @Test
+    public void testToCheckConsistencyProtoWithDataBoost() {
+        ConsistencyRequest consistencyRequest = ConsistencyRequest.getDataBoostConsistencyRequest(TABLE_ID);
+
+        RequestContext requestContext = RequestContext.create(PROJECT_ID, INSTANCE_ID, TABLE_ID);
+
+        CheckConsistencyRequest checkConsistencyRequest = consistencyRequest.toCheckConsistencyProto(requestContext, CONSISTENCY_TOKEN);
+
+        assertThat(checkConsistencyRequest.getName().equals(TABLE_ID));
+        assertThat(checkConsistencyRequest.getConsistencyToken().equals(CONSISTENCY_TOKEN));
+        assertThat(checkConsistencyRequest.getModeCase().equals(CheckConsistencyRequest.ModeCase.DATA_BOOST_READ_LOCAL_WRITES));
+    }
+
+    @Test
+    public void testToGenerateTokenProto() {
+        ConsistencyRequest consistencyRequest = ConsistencyRequest.getDataBoostConsistencyRequest(TABLE_ID);
+
+        RequestContext requestContext = RequestContext.create(PROJECT_ID, INSTANCE_ID, TABLE_ID);
+
+        GenerateConsistencyTokenRequest generateRequest = consistencyRequest.toGenerateTokenProto(requestContext);
+
+        assertThat(generateRequest.getName().equals(TABLE_ID));
+    }
+}
