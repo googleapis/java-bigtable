2b541c81e03ef2c1494e407aa0119a71007d226c
André Barbosa Sampaio <andrebsamp@gmail.com>
2025-01-29 17:47:37 -0500

    Add known conformance test failures

--- .kokoro/conformance.sh
+++ .kokoro/conformance.sh
@@ -69,7 +69,7 @@ do
   cd cloud-bigtable-clients-test/tests
   # If there is known failures, please add
   # "-skip `cat ../../test-proxy/known_failures.txt`" to the command below.
-  eval "go test -v -proxy_addr=:9999 ${configFlag}"
+  eval "go test -v -proxy_addr=:9999 ${configFlag} -skip `cat ../../test-proxy/known_failures.txt`"
   returnCode=$?
   popd
 
