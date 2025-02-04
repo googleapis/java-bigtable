a3c0d036cae802e44506d7d406085b6ed7d22196
Kevin Kim <kevkim@google.com>
2025-02-04 10:49:20 -0500

    revert formatting

--- google-cloud-bigtable/src/main/java/com/google/cloud/bigtable/admin/v2/models/AuthorizedView.java
+++ google-cloud-bigtable/src/main/java/com/google/cloud/bigtable/admin/v2/models/AuthorizedView.java
@@ -16,38 +16,31 @@
 
 package com.google.cloud.bigtable.admin.v2.models;
 
+import javax.annotation.Nonnull;
+
 import com.google.api.core.InternalApi;
 import com.google.api.core.InternalExtensionOnly;
 import com.google.bigtable.admin.v2.AuthorizedViewName;
 import com.google.common.base.Objects;
 import com.google.common.base.Preconditions;
-import javax.annotation.Nonnull;
 
 /**
- * A class that wraps the {@link com.google.bigtable.admin.v2.AuthorizedView}
- * protocol buffer
+ * A class that wraps the {@link com.google.bigtable.admin.v2.AuthorizedView} protocol buffer
  * object.
  *
- * <p>
- * An AuthorizedView represents subsets of a particular table based on rules.
- * The access to each
+ * <p>An AuthorizedView represents subsets of a particular table based on rules. The access to each
  * AuthorizedView can be configured separately from the Table.
  *
- * <p>
- * Users can perform read/write operation on an AuthorizedView by providing an
- * authorizedView id
- * besides a table id, in which case the semantics remain identical as
- * reading/writing on a Table
- * except that visibility is restricted to the subset of the Table that the
- * AuthorizedView
+ * <p>Users can perform read/write operation on an AuthorizedView by providing an authorizedView id
+ * besides a table id, in which case the semantics remain identical as reading/writing on a Table
+ * except that visibility is restricted to the subset of the Table that the AuthorizedView
  * represents.
  */
 public final class AuthorizedView {
   private final com.google.bigtable.admin.v2.AuthorizedView proto;
 
   /**
-   * Wraps the protobuf. This method is considered an internal implementation
-   * detail and not meant
+   * Wraps the protobuf. This method is considered an internal implementation detail and not meant
    * to be used by applications.
    */
   @InternalApi
@@ -69,7 +62,7 @@ public final class AuthorizedView {
     // Constructor ensures that name is not null.
     AuthorizedViewName fullName = AuthorizedViewName.parse(proto.getName());
 
-    // noinspection ConstantConditions
+    //noinspection ConstantConditions
     return fullName.getAuthorizedView();
   }
 
@@ -78,7 +71,7 @@ public final class AuthorizedView {
     // Constructor ensures that name is not null.
     AuthorizedViewName fullName = AuthorizedViewName.parse(proto.getName());
 
-    // noinspection ConstantConditions
+    //noinspection ConstantConditions
     return fullName.getTable();
   }
 
@@ -87,10 +80,7 @@ public final class AuthorizedView {
     return proto.getDeletionProtection();
   }
 
-  /**
-   * Gets the type of this authorized view, which currently can only be a subset
-   * view.
-   */
+  /** Gets the type of this authorized view, which currently can only be a subset view. */
   public AuthorizedViewType getAuthorizedViewType() {
     if (proto.hasSubsetView()) {
       return SubsetView.fromProto(proto.getSubsetView());
@@ -101,8 +91,7 @@ public final class AuthorizedView {
   }
 
   /**
-   * Creates the request protobuf. This method is considered an internal
-   * implementation detail and
+   * Creates the request protobuf. This method is considered an internal implementation detail and
    * not meant to be used by applications.
    */
   @InternalApi
@@ -128,11 +117,9 @@ public final class AuthorizedView {
   }
 
   /**
-   * Represents a subset of a Table. Please check the implementations of this
-   * interface for more
+   * Represents a subset of a Table. Please check the implementations of this interface for more
    * details.
    */
   @InternalExtensionOnly
-  public interface AuthorizedViewType {
-  }
+  public interface AuthorizedViewType {}
 }
