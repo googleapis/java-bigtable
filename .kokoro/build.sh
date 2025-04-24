e292f66f911d1c23687b01b2f2cc2f729cf499bb
Lawrence Qiu <lawrenceqiu@google.com>
2025-04-24 13:22:25 -0400

    chore: Resolve merge conflicts

--- .kokoro/build.sh
+++ .kokoro/build.sh
@@ -63,7 +63,7 @@ javadoc)
 integration)
     mvn -B ${INTEGRATION_TEST_ARGS} \
       -ntp \
-      -Dprotobuf.version=4.29.3 \
+      -Dprotobuf.version=4.29.4 \
       -Penable-integration-tests \
       -DtrimStackTrace=false \
       -Dclirr.skip=true \
