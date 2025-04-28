aa25a64ca6753a7b2096eda849d48bb73e621c72
Diego Marquez <diegomarquezp@google.com>
2025-04-28 11:55:39 -0400

    chore: enable verbose logs in it setup

--- .kokoro/build.sh
+++ .kokoro/build.sh
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -eo pipefail
+set -exo pipefail
 
 ## Get the directory of the build script
 scriptDir=$(realpath $(dirname "${BASH_SOURCE[0]}"))
