780cde4e209cafbb7ce77de960adc6c652d7da88
Lawrence Qiu <lawrenceqiu@google.com>
2025-08-26 14:18:10 -0400

    Revert "chore: generate libraries at Tue Aug 26 17:53:54 UTC 2025"
    This reverts commit b3f135bdd9a94abddf4082b7ac0e6c1c8b908f98.

--- .kokoro/build.sh
+++ .kokoro/build.sh
@@ -63,6 +63,7 @@ javadoc)
 integration)
     mvn -B ${INTEGRATION_TEST_ARGS} \
       -ntp \
+      -Dprotobuf.version=4.32.0 \
       -Penable-integration-tests \
       -DtrimStackTrace=false \
       -Dclirr.skip=true \
